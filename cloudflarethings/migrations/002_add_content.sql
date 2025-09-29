f2bbf8d4cc0c0fd1c03dd24ec796df2d16d60431
rubbishhaha <xiaolaji444@gmail.com>
2025-09-29 14:23:31 +0000

    s

---
+++ cloudflarethings/migrations/002_add_content.sql
@@ -0,0 +1,7 @@
+
+    LOAD DATA INFILE 'migrations/DSE.csv'
+    --INTO TABLE dse_trends
+    --FIELDS TERMINATED BY ','
+    -- ENCLOSED BY '"'
+    --LINES TERMINATED BY '\n'
+    --IGNORE 3 ROWS; -- Skip header row if present
